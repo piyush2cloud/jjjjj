b59f231f47dfc318779fa2ecb4afd90d2dd9fb6d
Piyush Aggarwal <piyush@viewlift.com>
2021-01-06 03:02:00 +0530

    first

---
+++ build/precache-manifest.6f0078e30cde145554917ae4a6a4e506.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3561e401b67d7bc9dbecc98c39864df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0edf7c9ac8fc98012f56",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "d4f853a0a7aa9e54aa4d",
+    "url": "/static/css/main.8bcbe668.chunk.css"
+  },
+  {
+    "revision": "0edf7c9ac8fc98012f56",
+    "url": "/static/js/2.ff90a4dd.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/static/js/2.ff90a4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f853a0a7aa9e54aa4d",
+    "url": "/static/js/main.764141fe.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2a7baf337e710fb09ed21dc3f1f0412f",
+    "url": "/static/media/11.2a7baf33.png"
+  },
+  {
+    "revision": "bc0c0bac7cac07bb8adf4943cb1a88c3",
+    "url": "/static/media/22.bc0c0bac.png"
+  },
+  {
+    "revision": "9ee0ae2f893c0556f65d3a285435dc9e",
+    "url": "/static/media/33.9ee0ae2f.png"
+  },
+  {
+    "revision": "41a42ddcd7b7ca53f0fd5e4eeefe4af9",
+    "url": "/static/media/44.41a42ddc.png"
+  },
+  {
+    "revision": "91e7abf2082ae4a59f45462be8e7b3a9",
+    "url": "/static/media/55.91e7abf2.png"
+  },
+  {
+    "revision": "02741fdba17f905e9edfc511fa8a676f",
+    "url": "/static/media/cric1.02741fdb.png"
+  },
+  {
+    "revision": "edce7317e2a22a90327ac69efdedcc33",
+    "url": "/static/media/cric2.edce7317.png"
+  },
+  {
+    "revision": "273cf41fcd6e329bb2d69db08ee56198",
+    "url": "/static/media/foot1.273cf41f.jpg"
+  },
+  {
+    "revision": "1dd32e14a8fb85f216bb027fa569d9eb",
+    "url": "/static/media/logo.1dd32e14.jpg"
+  },
+  {
+    "revision": "e3873f269f4ab06f6c0a3b7866eb816d",
+    "url": "/static/media/main.e3873f26.jpg"
+  },
+  {
+    "revision": "e5ba4ec5e7d6b38b1738a574364a3d05",
+    "url": "/static/media/ngtv.e5ba4ec5.mp4"
+  }
+]);
